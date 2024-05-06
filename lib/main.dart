05803d44a1b7383a235ae49d2f0a53f9108e9411
Oordii <kvandrushchenko@gmail.com>
2024-05-06 22:47:38 +0200

    add localization, add settings state provider, add themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,79 +1,65 @@
-import 'package:copy_todo_mvc/components/new_todo.dart';
-import 'package:copy_todo_mvc/components/todo_items.dart';
+import 'package:copy_todo_mvc/cubits/settings_cubit.dart';
 import 'package:copy_todo_mvc/cubits/todo_list_cubit.dart';
+import 'package:copy_todo_mvc/router/app_route.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
-import 'components/todo_bottom_bar.dart';
 import 'models/task.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+
   await Hive.initFlutter();
   Hive.registerAdapter(TaskAdapter());
   await Hive.openBox<Task>('tasks');
-  runApp(const MyApp());
+  await Hive.openBox('settings');
+
+  runApp(EasyLocalization(
+    supportedLocales: const [Locale('en', 'US'), Locale('ru', 'RU')],
+    path: "assets/translations",
+    fallbackLocale: const Locale('en', 'US'),
+    startLocale: const Locale('en', 'US'),
+    child: MyApp(),
+    ));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  final _appRouter = AppRouter();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'TodoMVC copy',
-      home: BlocProvider(
-          create: (context) => TodoListCubit(), child: const MyHomePage()),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color.fromARGB(245, 245, 245, 255),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(0, 25, 0, 0),
-        child: SingleChildScrollView(
-          child: Container(
-            alignment: Alignment.topCenter,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const Text(
-                  "todos",
-                  style: TextStyle(
-                      color: Color(0xffb83f45),
-                      fontWeight: FontWeight.w200,
-                      fontSize: 80,
-                      fontFamily: "Helvetica Neue"),
-                ),
-                Container(
-                    constraints: const BoxConstraints(maxWidth: 550),
-                    child: const Card(
-                      margin: EdgeInsets.fromLTRB(0, 0, 0, 45),
-                      shape: BeveledRectangleBorder(),
-                      elevation: 3,
-                      color: Color.fromARGB(254, 254, 254, 255),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [NewTodo(), TodoItems(), TodoBottomBar()],
-                      ),
-                    )),
-                const Text("Double-click to edit a todo",
-                    style: TextStyle(fontSize: 12)),
-                const Text("Created by житомирські бойові слони",
-                    style: TextStyle(fontSize: 12)),
-                const Text("Part of plan of destroying russia",
-                    style: TextStyle(fontSize: 12)),
-              ],
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => TodoListCubit()),
+        BlocProvider(create: (context) => SettingsCubit())
+      ],
+      child: BlocBuilder<SettingsCubit, SettingsState>(
+        builder: (context, state) {
+          return MaterialApp.router(
+            localizationsDelegates: context.localizationDelegates,
+            supportedLocales: context.supportedLocales,
+            locale: context.locale,
+            routerConfig: _appRouter.config(),
+            title: 'TodoMVC copy',
+            themeMode: state.themeMode,
+            theme: ThemeData(
+              useMaterial3: true,
+              brightness: Brightness.light,
+              colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(245, 255, 255, 255))
             ),
-          ),
-        ),
+            darkTheme: ThemeData(
+              useMaterial3: true,
+              brightness: Brightness.dark,
+              colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(1, 8, 5, 24), brightness: Brightness.dark)
+            ),
+            builder: (context, child) => child!,
+          );
+        }
       ),
     );
   }
