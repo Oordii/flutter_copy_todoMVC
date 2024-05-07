57984b61bb88c160950db61a751acc5e6ccc8e8a
Oordii <kvandrushchenko@gmail.com>
2024-05-07 15:00:12 +0200

    Implement task service

--- lib/cubits/todo_list_cubit.dart
+++ lib/cubits/todo_list_cubit.dart
@@ -1,68 +1,48 @@
 import 'package:copy_todo_mvc/models/bar_index.dart';
 import 'package:copy_todo_mvc/models/task.dart';
+import 'package:copy_todo_mvc/services/task_service.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 part 'todo_list_state.dart';
 
 class TodoListCubit extends Cubit<TodoListState> {
+  final _taskService = TaskService();
+
   TodoListCubit() : super(TodoListState(taskEntries: <dynamic, Task>{})) {
     _initializeTasks();
   }
-
-  void _initializeTasks() async {
-    var box = Hive.box<Task>('tasks');
-    emit(TodoListState(taskEntries: box.toMap()));
+  
+  void _initializeTasks() {
+    emit(TodoListState(taskEntries: _taskService.getTaskEntries()));
   }
 
   addTask(String taskName) async {
-    Task task;
-    var box = Hive.box<Task>('tasks');
-    
-    task = Task(name: taskName, isCompleted: false);
-
-    await box.add(task);
-
+    final newEntryKey = await _taskService.addTask(taskName);
     emit(TodoListState(
-        taskEntries: box.toMap(), barIndex: state.barIndex, editedEntryKey: box.keys.last));
+        taskEntries: _taskService.getTaskEntries(), barIndex: state.barIndex, editedEntryKey: newEntryKey));
   }
 
   setEditedEntryKey(dynamic key){
-    var box = Hive.box<Task>('tasks');
-
-    emit(TodoListState(taskEntries: box.toMap(), barIndex: state.barIndex, editedEntryKey: key));
+    emit(TodoListState(taskEntries: _taskService.getTaskEntries(), barIndex: state.barIndex, editedEntryKey: key));
   }
 
   toggleAllTasks() async {
-    var box = Hive.box<Task>('tasks');
-    if (box.values.any((element) => !element.isCompleted)) {
-      box.toMap().forEach((key, value) async {
-        value.isCompleted = true;
-        await box.put(key, value);
-      });
-    } else {
-      box.toMap().forEach((key, value) async {
-        value.isCompleted = false;
-        await box.put(key, value);
-      });
-    }
-
+    _taskService.toggleAllTasks();
     emit(TodoListState(
-        taskEntries: box.toMap(), barIndex: state.barIndex));
+        taskEntries: _taskService.getTaskEntries(), barIndex: state.barIndex));
   }
 
   Future<void> updateTask(MapEntry<dynamic, Task> entry) async {
-    var box = Hive.box<Task>('tasks');
-    await box.put(entry.key, entry.value);
+    _taskService.updateTask(entry);
     emit(TodoListState(
-        taskEntries: box.toMap(), barIndex: state.barIndex));
+        taskEntries: _taskService.getTaskEntries(), barIndex: state.barIndex));
   }
 
   Future<void> deleteTask(MapEntry<dynamic, Task> entry) async {
-    var box = Hive.box<Task>('tasks');
-    await box.delete(entry.key);
+    _taskService.deleteTask(entry);
     emit(TodoListState(
-        taskEntries: box.toMap(), barIndex: state.barIndex));
+        taskEntries: _taskService.getTaskEntries(), barIndex: state.barIndex));
   }
 
   void setBarItemIndex(BarIndex index) {
@@ -72,12 +52,7 @@ class TodoListCubit extends Cubit<TodoListState> {
   }
 
   void clearCompletedTasks() async {
-    var box = Hive.box<Task>('tasks');
-    box.toMap().forEach((key, value) async {
-      if (value.isCompleted) {
-        await box.delete(key);
-      }
-    });
-    emit(TodoListState(taskEntries: box.toMap()));
+    _taskService.clearCompleted();
+    emit(TodoListState(taskEntries: _taskService.getTaskEntries()));
   }
 }
---
+++ lib/services/task_service.dart
@@ -0,0 +1,49 @@
+
+import 'package:copy_todo_mvc/models/task.dart';
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+
+class TaskService {
+  TaskService();
+
+  final _box = Hive.box<Task>('tasks');
+
+  Map<dynamic, Task> getTaskEntries() {
+    return _box.toMap();
+  }
+
+  Future<int> addTask(String taskName) async {
+    final task = Task(name: taskName, isCompleted: false);
+    return await _box.add(task);
+  }
+
+  void toggleAllTasks() async {
+    if (_box.values.any((element) => !element.isCompleted)) {
+      _box.toMap().forEach((key, value) async {
+        value.isCompleted = true;
+        await _box.put(key, value);
+      });
+    } else {
+      _box.toMap().forEach((key, value) async {
+        value.isCompleted = false;
+        await _box.put(key, value);
+      });
+    }
+  }
+
+  void updateTask(MapEntry<dynamic, Task> entry) async {
+    await _box.put(entry.key, entry.value);
+  }
+
+  void deleteTask(MapEntry<dynamic, Task> entry) async {
+    await _box.delete(entry.key);
+  }
+
+  void clearCompleted() async {
+    _box.toMap().forEach((key, value) async {
+      if (value.isCompleted) {
+        await _box.delete(key);
+      }
+    });
+  }
+}
