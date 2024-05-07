613ebbd3b6ab325fe5313861e4750b9601871d21
Oordii <kvandrushchenko@gmail.com>
2024-05-07 14:40:13 +0200

    add edited task key

--- lib/cubits/todo_list_state.dart
+++ lib/cubits/todo_list_state.dart
@@ -1,7 +1,8 @@
 part of 'todo_list_cubit.dart';
 
 class TodoListState {
-  TodoListState({required this.taskEntries, this.barIndex = BarIndex.all});
+  TodoListState({required this.taskEntries, this.barIndex = BarIndex.all, this.editedEntryKey});
   final Map<dynamic, Task> taskEntries;
   BarIndex barIndex;
+  dynamic editedEntryKey;
 }
