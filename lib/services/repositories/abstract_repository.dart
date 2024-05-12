9bb08f5600a261d3d6c99bc39d90c2cea7c5fd98
Oordii <kvandrushchenko@gmail.com>
2024-05-12 17:58:23 +0200

    add repository interface

---
+++ lib/services/repositories/abstract_repository.dart
@@ -0,0 +1,12 @@
+import 'package:copy_todo_mvc/models/task.dart';
+import 'package:flutter/material.dart';
+
+abstract class Repository {
+  Future<void> setTheme(ThemeMode mode);
+  Future<ThemeMode> getTheme();
+  List<Task> getAllTasks();
+  Future<void> addTask(Task task);
+  Task getTaskById(int id);
+  Future<void> updateTask(Task task);
+  Future<void> deleteTask(Task task);
+}
