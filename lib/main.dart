d1e41930abe16b76cd629c5902bda60fc8f3c483
Oordii <kvandrushchenko@gmail.com>
2024-05-12 17:59:52 +0200

    add dependency injection

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,25 @@ import 'package:copy_todo_mvc/cubits/settings_cubit.dart';
 import 'package:copy_todo_mvc/cubits/todo_list_cubit.dart';
 import 'package:copy_todo_mvc/models/global_theme.dart';
 import 'package:copy_todo_mvc/router/app_route.dart';
+import 'package:copy_todo_mvc/services/repositories/abstract_repository.dart';
+import 'package:copy_todo_mvc/services/repositories/hive_repository.dart';
+import 'package:copy_todo_mvc/services/settings_service.dart';
+import 'package:copy_todo_mvc/services/task_service.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 import 'models/task.dart';
+
+final getIt = GetIt.instance;
+
+void setup() {
+  getIt.registerSingleton<Repository>(HiveRepository());
+  getIt.registerSingleton<TaskService>(TaskService());
+  getIt.registerSingleton<SettingsService>(SettingsService());
+}
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,6 +35,8 @@ Future<void> main() async {
     Hive.openBox<Task>('tasks');
   }
   await Hive.openBox('settings');
+
+  setup();
 
   runApp(EasyLocalization(
     supportedLocales: const [Locale('en', 'US'), Locale('ru', 'RU')],
