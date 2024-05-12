09e80058fa3611bb447d8d40f05b8bfb6929a5f2
Oordii <kvandrushchenko@gmail.com>
2024-05-12 18:01:46 +0200

    add default font family, add static colors

---
+++ lib/models/app_color.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColor{
+  AppColor._();
+  static const Color titleRed = Color(0xffb83f45);
+}
--- lib/models/global_theme.dart
+++ lib/models/global_theme.dart
@@ -1,79 +1,77 @@
-
+import 'package:copy_todo_mvc/models/app_color.dart';
 import 'package:flutter/material.dart';
 
 class GlobalTheme {
-  static ThemeData light(){
+  GlobalTheme._();
+  
+  static ThemeData light() {
     return ThemeData(
       useMaterial3: true,
       brightness: Brightness.light,
-      colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(245, 255, 255, 255)),
+      colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(245, 255, 255, 255)),
+      fontFamily: "Helvetica Neue",
       textButtonTheme: const TextButtonThemeData(
         style: ButtonStyle(
           textStyle: MaterialStatePropertyAll<TextStyle>(
             TextStyle(
               fontWeight: FontWeight.w400,
               fontSize: 16,
-              fontFamily: "Helvetica Neue",
               backgroundColor: null,
               wordSpacing: 1,
               decorationThickness: 1,
-            )
-          )
-        )
+            ),
+          ),
+        ),
       ),
       listTileTheme: const ListTileThemeData(
         titleTextStyle: TextStyle(
           color: Colors.black54,
           fontWeight: FontWeight.w400,
           fontSize: 24,
-          fontFamily: "Helvetica Neue"
-        )
+        ),
       ),
       textTheme: const TextTheme(
         displayLarge: TextStyle(
-          color: Color(0xffb83f45),
+          color: AppColor.titleRed,
           fontWeight: FontWeight.w200,
           fontSize: 48,
-          fontFamily: "Helvetica Neue"
         ),
         displaySmall: TextStyle(
-          color: Color(0xffb83f45),
+          color: AppColor.titleRed,
           fontWeight: FontWeight.w400,
           fontSize: 20,
-          fontFamily: "Helvetica Neue"
         ),
         bodyMedium: TextStyle(
           color: Colors.black87,
           fontWeight: FontWeight.w500,
           fontSize: 20,
-          fontFamily: "Helvetica Neue"
         ),
         bodySmall: TextStyle(
           color: Colors.black87,
           fontWeight: FontWeight.w400,
           fontSize: 16,
-          fontFamily: "Helvetica Neue"
         ),
         labelMedium: TextStyle(
           fontWeight: FontWeight.w400,
           fontSize: 16,
-          fontFamily: "Helvetica Neue"
         ),
         labelSmall: TextStyle(
           color: Colors.black87,
           fontWeight: FontWeight.w200,
           fontSize: 16,
-          fontFamily: "Helvetica Neue"
-        ), 
-      )
+        ),
+      ),
     );
   }
 
-  static ThemeData dark(){
+  static ThemeData dark() {
     return ThemeData(
       useMaterial3: true,
       brightness: Brightness.dark,
-      colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(1, 13, 15, 37), brightness: Brightness.dark),
+      colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(1, 13, 15, 37),
+          brightness: Brightness.dark),
       disabledColor: Colors.white10,
       textButtonTheme: const TextButtonThemeData(
         style: ButtonStyle(
@@ -81,59 +79,51 @@ class GlobalTheme {
             TextStyle(
               fontWeight: FontWeight.w400,
               fontSize: 16,
-              fontFamily: "Helvetica Neue",
               backgroundColor: null,
               wordSpacing: 1,
               decorationThickness: 1,
-            )
-          )
-        )
+            ),
+          ),
+        ),
       ),
       listTileTheme: const ListTileThemeData(
         titleTextStyle: TextStyle(
           color: Colors.white60,
           fontWeight: FontWeight.w400,
           fontSize: 24,
-          fontFamily: "Helvetica Neue"
-        )
+        ),
       ),
       textTheme: const TextTheme(
         displayLarge: TextStyle(
-          color: Color(0xffb83f45),
+          color: AppColor.titleRed,
           fontWeight: FontWeight.w200,
           fontSize: 48,
-          fontFamily: "Helvetica Neue"
         ),
         displaySmall: TextStyle(
-          color: Color(0xffb83f45),
+          color: AppColor.titleRed,
           fontWeight: FontWeight.w400,
           fontSize: 20,
-          fontFamily: "Helvetica Neue"
         ),
         bodyMedium: TextStyle(
           color: Colors.white70,
           fontWeight: FontWeight.w500,
           fontSize: 20,
-          fontFamily: "Helvetica Neue"
         ),
         bodySmall: TextStyle(
           color: Colors.white70,
           fontWeight: FontWeight.w400,
           fontSize: 16,
-          fontFamily: "Helvetica Neue"
         ),
         labelMedium: TextStyle(
           fontWeight: FontWeight.w400,
           fontSize: 16,
-          fontFamily: "Helvetica Neue"
         ),
         labelSmall: TextStyle(
           color: Colors.white54,
           fontWeight: FontWeight.w200,
           fontSize: 16,
-          fontFamily: "Helvetica Neue"
-        )
-      )
+        ),
+      ),
     );
   }
 }
