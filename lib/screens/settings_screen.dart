1429c3c5e1a29be46f6da0578bcd7bd4be049b53
Oordii <kvandrushchenko@gmail.com>
2024-05-14 21:58:04 +0200

    add sign out button

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,6 +1,8 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:copy_todo_mvc/cubits/settings_cubit.dart';
+import 'package:copy_todo_mvc/router/app_route.gr.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -17,8 +19,7 @@ class SettingsScreen extends StatefulWidget {
 class _SettingsScreenState extends State<SettingsScreen> {
   @override
   Widget build(BuildContext context) {
-    return (
-      Scaffold(
+    return (Scaffold(
         appBar: AppBar(
           title: Text("settings".tr(), style: const TextStyle(fontSize: 24)),
           elevation: 3,
@@ -26,7 +27,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
         body: Align(
           alignment: Alignment.topCenter,
           child: Container(
-              constraints: const BoxConstraints(maxWidth: 800, maxHeight: 200),
+              constraints: const BoxConstraints(maxWidth: 800, maxHeight: 240),
               child: Card(
                 margin: const EdgeInsets.fromLTRB(16, 40, 16, 0),
                 elevation: 3,
@@ -36,30 +37,35 @@ class _SettingsScreenState extends State<SettingsScreen> {
                     mainAxisAlignment: MainAxisAlignment.spaceAround,
                     crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: [
-                      Builder(
-                        builder: (context) {
-                          final settingsState = context.watch<SettingsCubit>().state;
-                          return Row(
-                            mainAxisSize: MainAxisSize.max,
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Row(
-                                children: [
-                                  const Icon(Icons.dark_mode),
-                                  Padding(
-                                    padding: const EdgeInsets.only(left: 8),
-                                    child: Text("settings_theme".tr(), style: Theme.of(context).textTheme.bodyMedium),
-                                  ),
-                                ],
-                              ),
-                              Switch(value: settingsState.themeMode == ThemeMode.dark, 
-                              onChanged: (value) {
-                                context.read<SettingsCubit>().setTheme(value ? ThemeMode.dark : ThemeMode.light);
-                              })
-                            ],
-                          );
-                        }
-                      ),
+                      Builder(builder: (context) {
+                        final settingsState =
+                            context.watch<SettingsCubit>().state;
+                        return Row(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                const Icon(Icons.dark_mode),
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 8),
+                                  child: Text("settings_theme".tr(),
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .bodyMedium),
+                                ),
+                              ],
+                            ),
+                            Switch(
+                                value:
+                                    settingsState.themeMode == ThemeMode.dark,
+                                onChanged: (value) {
+                                  context.read<SettingsCubit>().setTheme(
+                                      value ? ThemeMode.dark : ThemeMode.light);
+                                })
+                          ],
+                        );
+                      }),
                       Row(
                         mainAxisSize: MainAxisSize.max,
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -69,29 +75,50 @@ class _SettingsScreenState extends State<SettingsScreen> {
                               const Icon(Icons.language),
                               Padding(
                                 padding: const EdgeInsets.only(left: 8),
-                                child: Text("settings_locale".tr(), style: Theme.of(context).textTheme.bodyMedium),
+                                child: Text("settings_locale".tr(),
+                                    style:
+                                        Theme.of(context).textTheme.bodyMedium),
                               ),
                             ],
                           ),
                           DropdownButton<Locale>(
-                            value: context.locale,
-                            items: [
-                              DropdownMenuItem(value: const Locale('en', 'US'),child: Text("English", style:  Theme.of(context).textTheme.labelMedium),),
-                              DropdownMenuItem(value: const Locale('ru', 'RU'),child: Text("Русский", style:  Theme.of(context).textTheme.labelMedium),)
-                            ],
-                            onChanged: (locale){
-                              setState(() {
-                                context.setLocale(locale!);
-                              });
-                            })
+                              value: context.locale,
+                              items: [
+                                DropdownMenuItem(
+                                  value: const Locale('en', 'US'),
+                                  child: Text("English",
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .labelMedium),
+                                ),
+                                DropdownMenuItem(
+                                  value: const Locale('ru', 'RU'),
+                                  child: Text("Русский",
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .labelMedium),
+                                )
+                              ],
+                              onChanged: (locale) {
+                                setState(() {
+                                  context.setLocale(locale!);
+                                });
+                              })
                         ],
-                      )
+                      ),
+                      TextButton(
+                        onPressed: () async {
+                          await FirebaseAuth.instance.signOut();
+                          if (context.mounted){
+                            await context.router.replaceAll([const SignInRoute()]);
+                          }
+                        },
+                        child: const Text("Sign out"),
+                      ),
                     ],
                   ),
                 ),
               )),
-        )
-      )
-    );
+        )));
   }
 }
