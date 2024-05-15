f603b5a6d1de63c10b8fec9d605c35559abbe9ee
Oordii <kvandrushchenko@gmail.com>
2024-05-15 17:08:56 +0200

    add firestore suport

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:hive_flutter/adapters.dart';
@@ -6,17 +7,20 @@ part 'task.freezed.dart';
 part 'task.g.dart';
 
 @freezed
-class Task with _$Task{
+class Task with _$Task {
   @HiveType(typeId: 1, adapterName: 'TaskAdapter')
   const factory Task({
-    @HiveField(2)
-    required int id,
-    @HiveField(0)
-    required String name,
-    @HiveField(1)
-    @Default(false)
-    bool isCompleted,
+    @HiveField(2) required int id,
+    @HiveField(0) required String name,
+    @HiveField(1) @Default(false) bool isCompleted,
   }) = _Task;
 
- factory Task.fromJson(Map<String, dynamic> json) => _$TaskFromJson(json);
+  factory Task.fromJson(Map<String, dynamic> json) => _$TaskFromJson(json);
+
+  factory Task.fromFirestore(
+          DocumentSnapshot snapshot, SnapshotOptions? options) =>
+      Task.fromJson(snapshot.data() as Map<String, dynamic>);
+
+  static Map<String, Object?> toFirestore(Task task, SetOptions? options) =>
+      task.toJson();
 }
