d13c9b2008adac59310d37fa19f4f6721862e8d1
Oordii <kvandrushchenko@gmail.com>
2024-05-17 20:14:22 +0200

    set stornmg type to bar index

--- lib/cubits/todo_list_state.dart
+++ lib/cubits/todo_list_state.dart
@@ -6,7 +6,7 @@ class TodoListState with _$TodoListState{
   const factory TodoListState.loading() = _Loading;
   const factory TodoListState.success({ 
     required List<Task> tasks, 
-    @Default(BarIndex.all) barIndex, 
+    @Default(BarIndex.all) BarIndex  barIndex, 
     @Default(null) int? editedTaskId}) = _Success;
   const factory TodoListState.error(String errorMessage) = _Error;
 }
