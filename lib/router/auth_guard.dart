b6df4ba9dc610bc0be5602423174aae9f75128d8
Oordii <kvandrushchenko@gmail.com>
2024-05-18 02:07:08 +0200

    redirect to sign in screen if not authenticated

--- lib/router/auth_guard.dart
+++ lib/router/auth_guard.dart
@@ -1,18 +1,26 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:copy_todo_mvc/main.dart';
+import 'package:copy_todo_mvc/router/app_route.dart';
 import 'package:copy_todo_mvc/router/app_route.gr.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthGuard extends AutoRouteGuard {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
-  AuthGuard();
+  AuthGuard(){
+    _firebaseAuth.authStateChanges().listen((user){
+      if(user != null){
+        getIt.get<AppRouter>().replaceAll([const SignInRoute()]);
+      }
+    });
+  }
 
   @override
   void onNavigation(NavigationResolver resolver, StackRouter router) {
     if (_firebaseAuth.currentUser != null) {
       resolver.next();
     } else {
-      router.push(const SignInRoute());
+      router.replaceAll([const SignInRoute()]);
     }
   }
 }
